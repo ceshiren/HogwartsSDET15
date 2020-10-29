a93d3480c39b019c09457c1a07873bcf49257412
xj418974188 <xujuan418974188@163.com>
2020-10-29 21:46:31 +0800

    app 企业微信实战1 自动打卡功能

---
+++ app/test_demo.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This sample code uses the Appium python client
+# pip install Appium-Python-Client
+# Then you can paste this into a file and simply run with Python
+from time import sleep
+
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class TestDemo:
+
+    def setup(self):
+        # 定义了一个字典
+        caps = {}
+        caps["platformName"] = "Android"
+        caps["deviceName"] = "hogwarts"
+        caps["appPackage"] = "com.tencent.wework"
+        caps["appActivity"] = ".launch.LaunchSplashActivity"
+        caps["noReset"] = "True"
+        # caps["settings[waitForIdleTimeout]"] = 0
+        # 关键  localhost:4723  本机ip:server端口
+        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+        self.driver.implicitly_wait(5)
+
+    def test_demo(self):
+        el1 = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.RelativeLayout[2]/android.widget.TextView")
+        el1.click()
+        el2 = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.ListView/android.widget.RelativeLayout[7]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
+        el2.click()
+
+    def test_daka(self):
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='工作台']").click()
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+                                 'new UiScrollable(new UiSelector()\
+                                 .scrollable(true).instance(0))\
+                                 .scrollIntoView(new UiSelector()\
+                                 .text("打卡").instance(0));').click()
+        # settings
+        self.driver.update_settings({"waitForIdleTimeout": 0})
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='外出打卡']").click()
+        self.driver.find_element(MobileBy.XPATH, '//*[contains(@text, "次外出")]').click()
+        # sleep(2)
+        # assert "外出打卡成功" in self.driver.page_source
+        # 显式等待
+        WebDriverWait(self.driver, 10).until(lambda x: "外出打卡成功" in x.page_source)
+
+    def teardown(self):
+        self.driver.quit()
